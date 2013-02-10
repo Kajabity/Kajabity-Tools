691aa529038c05d74553b978e15891405cfd3348
Kajabity <simon@kajabity.com>
2013-02-10 00:18:12 +0000

    Committing first version of Kajabit Tools for C#

---
+++ Kajabity Tools/Properties/AssemblyInfo.cs
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2009 Williams Technologies Limtied.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * Kajbity is a trademark of Williams Technologies Limited.
+ * 
+ * http://www.kajabity.com
+ */
+
+#region Using directives
+
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+#endregion
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Kajabity Tools")]
+[assembly: AssemblyDescription("A collection of utility classes.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Williams Technologies Limited")]
+[assembly: AssemblyProduct("Kajabity Tools")]
+[assembly: AssemblyCopyright("Copyright 2009-13")]
+[assembly: AssemblyTrademark("Williams Technologies Limited")]
+[assembly: AssemblyCulture("")]
+
+// This sets the default COM visibility of types in the assembly to invisible.
+// If you need to expose a type to COM, use [ComVisible(true)] on that type.
+[assembly: ComVisible(true)]
+
+// The assembly version has following format :
+//
+// Major.Minor.Build.Revision
+//
+// You can specify all the values or you can use the default the Revision and 
+// Build Numbers by using the '*' as shown below:
+[assembly: AssemblyVersion("0.1.*")]
+[assembly: NeutralResourcesLanguageAttribute("en-GB")]
